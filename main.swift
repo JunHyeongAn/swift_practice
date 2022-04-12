5bc89ea4eae5991cd2cc954440a01d231bc5943a
Jun Hyeong An <99971427+JunHyeongAn@users.noreply.github.com>
2022-04-12 03:21:11 +0000

    자료형

--- main.swift
+++ main.swift
@@ -1,4 +1,23 @@
-let age: Int = 10;
-print("my age is \(age)");
-print("#####################");
-dump(age);
+let age: Int = 10
+print("my age is \(age)")
+print("#####################")
+dump(age)
+///////////////////////////////////
+
+var someBool: Bool = true
+someBool = false
+// someBool = 1;  // 불가능
+// someBool = 0;  // 불가능
+
+var someDouble = 3.14
+someDouble = 3 // 가능
+
+var someFloat = 3.14
+someFloat = 3 // 가능
+
+var someCharacter = "A"
+
+var someString = "Hello, world"
+someString = "안녕하세요"
+
+print(someString)
