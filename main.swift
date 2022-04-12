11eeee99a765f4a7ce5e8b15ae9e76b54486ea79
Jun Hyeong An <99971427+JunHyeongAn@users.noreply.github.com>
2022-04-12 04:45:56 +0000

    함수

--- main.swift
+++ main.swift
@@ -39,3 +39,23 @@ var mySet: Set<Int> = Set<Int>()
 mySet.insert(1)
 mySet.insert(5)
 print(mySet)
+
+////////////////////////////////
+func sum(a: Int, b: Int) -> Int {
+  return a + b
+}
+
+func printMyName(name: String) -> Void {
+  print(name)
+}
+
+printMyName(name: "준형")
+
+func greeting(friend: String, me: String) -> Void {
+  print("Hello \(friend). I'm \(me)")
+}
+// 위의 greeing과 다른 함수이다. 왜냐 전달인자 (to, from)도 함수이름
+// 으로 포함하기 때문에.
+func greeting(to freind: String, from me: String) -> Void {
+  print("Hello \(freind), I'm \(me)")
+}
